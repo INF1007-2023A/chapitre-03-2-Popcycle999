e2d3f63b873ed9476bff42a5910d75a13a57125d
Popcycle999 <128432971+Popcycle999@users.noreply.github.com>
2023-09-15 09:20:46 -0400

    update

--- exercice.py
+++ exercice.py
@@ -3,23 +3,42 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return voltage**2/resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
+	test_orthogonal = v1[0] * v2[0] + v1[1] * v2[1] 
+	if test_orthogonal == 0:
+		return True
 	pass
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	x= 0
+	y= 0
 	for v in values:
+		if v >= 0:
+			x += v
+			y += 1
 		pass # La variable v contient une valeur de la liste.
+	return x/y
 
 def bills(value):
+	twenties = value//20
+	reste1 = (value/20 - value//20)*20
+
+	tens = reste1//10
+	reste2 = (reste1/10 - reste1//10)*10
+
+	fives = reste2//5
+	reste3 = (reste2/5 - reste2//5)*5
+
+	twos = reste3//2
+	ones = round((reste3/2 - reste3//2)*2)
+
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, twos, ones)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -27,8 +46,12 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
+		for i in range (3):
+			abs_value - base**i
+			result += digit_letters[i]
 		pass
 	if value < 0:
+		result = "-" + result
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
 		pass
 	return result
